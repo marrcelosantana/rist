6d8fc965d6b17716b77c7dc0a0dc8abb1ff7d56c
marrcelosantana <marcelosantana@alu.ufc.br>
2023-08-22 16:01:56 -0300

    [RIST] Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./themes/default-theme";
 import { GlobalStyle } from "./styles/global";
 
+import { Header } from "./components/Header";
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Header />
     </ThemeProvider>
   );
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100%;
+  height: 64px;
+  background-color: ${({ theme }) => theme.COLORS.BLACK};
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 18px 64px;
+`;
+
+export const Image = styled.img``;
